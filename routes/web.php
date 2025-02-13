6d6173061ece13756898fda6cc15efd3c31acb9f
Diaa <info@dmohammad.com>
2025-02-13 11:45:23 +0100

    diaa: Authenticates user

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-require __DIR__.'/auth.php';
+
+Route::post('login', [AuthController::class, 'login']);
+
+//require __DIR__.'/auth.php';

1f259ba68e60d83f04ab5fa3352aa42f25c8ea35
Diaa <info@dmohammad.com>
2025-02-13 22:03:08 +0100

    diaa: Adds products list to api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +16,5 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     //Route::get('/users', [UserController::class, 'index']);
     Route::apiResource('orders', OrderController::class);
+    Route::get('products', [ProductController::class, 'index']);
 });
